e57bcb04795e562f2fbb40203b1ffbe80dbd5544
Anthony Nucci <anthnucci@hotmail.com>
2021-07-21 17:55:42 -0400

    finished the assignment making a console game of rock paper scissors

---
+++ README.md
@@ -0,0 +1,3 @@
+Project: Rock Paper Scissors, step 7 in javascript basics for The Odin Project.
+
+Creating the logic of rock paper scissors in console. 
--- script.js
+++ script.js
@@ -1,60 +1,83 @@
 
-// function to generate a random selection for computer
+// the function to play a game of 5 rounds
+function game() {
 
-let playerScore = 0;
-let computerScore = 0;
+    const games = 5;
+    let playerScore = 0;
+    let computerScore = 0;
+    
+// created loop for the game    
+    for (let i = 0; i < games; i++) {
 
-let playerSelection;
-let computerSelection;
+// the player input promt, creates the player choice
+        let playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
 
-const choices = ["rock", "paper", "scissors"];
+// computer choices       
+        const choices = ["rock", "paper", "scissors"];
 
-function computerPlay() {
+// created function to generate a computer choice and give the computer selection a value    
+    function computerPlay() {
     return choices[Math.floor(Math.random()*choices.length)];
+    }
+    let computerSelection = computerPlay();
+    
+// created results for each round using playround function and stored the score in each   
+    let round = playRound (playerSelection, computerSelection);
+    if(round === "You won!") {
+        playerScore++;
+        console.log("You won! " + "the score is " + playerScore + " to " + computerScore);
+    }
+    else if (round === "You lost") {
+        computerScore++;
+        console.log("You lost " + "the score is " + playerScore + " to " + computerScore);
+    }
+    else if (round === "You tied") {
+        console.log("You tied " + "the score is " + playerScore + " to " + computerScore);
+    }
 }
+    if(playerScore > computerScore) {
+        console.log("You beat the computer " + playerScore + " to " + computerScore);
+    }
+    else if (playerScore < computerScore) {
+        console.log("You lost to the computer " + playerScore + " to " + computerScore);
+    }
+    else console.log("You tied the game.")
+}
+//end of the game function
 
-function playRound() {
+
+// the function that compares theselections of the player and computer and returns a value to be used in game
+function playRound(playerSelection, computerSelection) {
     
     if (playerSelection.toLowerCase() == "rock".toLowerCase() && computerSelection == "rock") {
         return "You tied";
     }
-    if (playerSelection.toLowerCase() == "rock".toLowerCase() && computerSelection == "paper") {
-        computerScore++; return "You lost";
+    if (playerSelection == "rock" && computerSelection == "paper") {
+        return "You lost";
     }
-    if (playerSelection.toLowerCase() == "rock".toLowerCase() && computerSelection == "scissors") {
-        playerScore++; return "You won!";
+    if (playerSelection == "rock" && computerSelection == "scissors") {
+        return "You won!";
     }
-    if (playerSelection.toLowerCase() == "paper".toLowerCase() && computerSelection == "rock") {
-        playerScore++; return "You won!";
+    if (playerSelection == "paper" && computerSelection == "rock") {
+        return "You won!";
     }
-    if (playerSelection.toLowerCase() == "paper".toLowerCase() && computerSelection == "paper") {
+    if (playerSelection == "paper" && computerSelection == "paper") {
         return "You tied";
     }
-    if (playerSelection.toLowerCase() == "paper".toLowerCase() && computerSelection == "scissors") {
-        computerScore++; return "You lost";
+    if (playerSelection == "paper" && computerSelection == "scissors") {
+       return "You lost";
     }
-    if (playerSelection.toLowerCase() == "scissors".toLowerCase() && computerSelection == "rock") {
-        computerScore++; return "You lost";
+    if (playerSelection == "scissors" && computerSelection == "rock") {
+        return "You lost";
     }
-    if (playerSelection.toLowerCase() == "scissors".toLowerCase() && computerSelection == "paper") {
-        playerScore++; return "You won!";
+    if (playerSelection == "scissors" && computerSelection == "paper") {
+        return "You won!";
     }
-    if (playerSelection.toLowerCase() == "scissors".toLowerCase() && computerSelection == "scissors") {
+    if (playerSelection == "scissors" && computerSelection == "scissors") {
         return "You tied";
     }
 }
+// end of the playround function
 
-function game() {
+console.log(game())
 
-    let playerScore = 0;
-    let computerScore = 0;
-    
-    playRound()
-
-}
-
-
-
-
-
-console.log(playRound())
